736bbeed92a82a30f7313963daf0c4a1d74dc638
Sapna-Thakur <sapnathakur873609@gmail.com>
2026-08-17 16:16:06 +0530

    Create variable.c to demonstrate variable usage
    Fix variable names and format specifiers in printf.

---
+++ 01-Basics/variable.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+int main(void)
+{
+  int age =22;
+float cgpa =7.6f;
+char grade ='A';
+printf("Age :%d\n",Age);
+printf("cgpa :%d\n",cgpa);
+printf("grade :%d\n",grade);
+return 0;
+}
